f9ca47e4dab862fce5cff808b195826f3ec919af
Petr Gregor <petr.gregor@email.cz>
2024-05-28 20:08:48 +0200

    Movies - Countries, title_cz and length.

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from viewer.models import *
 
+admin.site.register(Country)
 admin.site.register(Genre)
 admin.site.register(Movie)
 
--- viewer/models.py
+++ viewer/models.py
@@ -18,10 +18,24 @@ class Genre(Model):
         return self.movies.all().count()
 
 
+class Country(Model):
+    name = CharField(max_length=64, null=False, blank=False)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Countries"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Movie(Model):
     title_orig = CharField(max_length=185, null=False, blank=False)  # https://cs.wikipedia.org/wiki/Lopadotemachoselachogaleokranioleipsanodrimhypotrimmatosilphioparaomelitokatakechymenokichlepikossyphophattoperisteralektryonoptekephalliokigklopeleiolagoiosiraiobaphetraganopterygon
+    title_cz = CharField(max_length=185, null=True, blank=False)
+    countries = ManyToManyField(Country, blank=True, related_name='movies')
     #genre = ForeignKey(Genre, on_delete=DO_NOTHING)
     genres = ManyToManyField(Genre, blank=True, related_name='movies')
+    length = IntegerField(null=True, blank=True)
     rating = IntegerField(null=True, blank=True)
     released = DateField()
     description = TextField(null=True, blank=True)
@@ -40,5 +54,11 @@ class Movie(Model):
         result = ""
         for genre in self.genres.all():
             result += f"{genre}, "
+        return result[:-2]
+
+    def print_countries(self):
+        result = ""
+        for country in self.countries.all():
+            result += f"{country}, "
         return result[:-2]
 
