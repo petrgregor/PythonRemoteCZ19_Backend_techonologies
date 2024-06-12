585feaa27fa26f48525a4f71cfda16338927f837
Petr Gregor <petr.gregor@email.cz>
2024-06-12 18:26:54 +0200

    Accounts - profile.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.db.transaction import atomic
+from django.forms import DateField, CharField, Textarea, NumberInput
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
+from accounts.models import Profile
 
 
 class SubmittableLoginView(LoginView):
@@ -13,9 +17,19 @@ class SignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'first_name', 'last_name', 'password1', 'password2']
 
+    date_of_birth = DateField(widget=NumberInput(attrs={'type': 'date'}))
+    biography = CharField(label='Tell us your story with movies.', widget=Textarea)
+
+    @atomic
     def save(self, commit=True):
-        self.instance.is_active = False
-        return super().save(commit)
+        self.instance.is_active = True
+        user = super().save(commit)  # creates instance of User
+        date_of_birth = self.cleaned_data['date_of_birth']  # take information about Profile
+        biography = self.cleaned_data['biography']
+        profile = Profile(user=user, date_of_birth=date_of_birth, biography=biography)  # creates instance of Profile
+        if commit:
+            profile.save()
+        return user
 
 
 class SignUpView(CreateView):
@@ -27,3 +41,11 @@ class SignUpView(CreateView):
 class SubmittablePasswordChangeView(PasswordChangeView):
     template_name = 'form.html'
     success_url = reverse_lazy('home')
+
+
+# TODO: ProfileListView - seznam všech profilů
+# TODO: ProfileDetailView - informace z konkrétního profilu
+# ProfileCreateView - nemusíme řešit - profil se vytváří při signup
+# TODO: ProfileUpdateView
+# TODO: ProfileDeleteView
+
